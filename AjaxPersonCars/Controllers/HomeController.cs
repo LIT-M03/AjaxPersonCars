c169909f8d691786ca468d44f8cbb03c664a00c9
Avrumi Friedman <info@lakewoodprogramming.com>
2016-02-21 12:51:40 -0500

    Initial Commit

---
+++ AjaxPersonCars/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxPersonCars.Data;
+
+namespace AjaxPersonCars.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPerson(Person person)
+        {
+            var manager = new PersonCarsManager(Properties.Settings.Default.ConStr);
+            manager.AddPerson(person);
+            return Json(person);
+        }
+
+        public ActionResult GetAllPeople()
+        {
+            var manager = new PersonCarsManager(Properties.Settings.Default.ConStr);
+            return Json(manager.GetAllPersons(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public void AddCar(Car car)
+        {
+            var manager = new PersonCarsManager(Properties.Settings.Default.ConStr);
+            manager.AddCar(car);
+        }
+
+        [HttpPost]
+        public ActionResult GetCarsForPerson(int personId)
+        {
+            var manager = new PersonCarsManager(Properties.Settings.Default.ConStr);
+            return Json(manager.GetCarsForPerson(personId), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public void DeletePerson(int personId)
+        {
+            var manager = new PersonCarsManager(Properties.Settings.Default.ConStr);
+            manager.DeletePersonAndCars(personId);
+        }
+
+    }
+
+}
---
+++ AjaxPersonCars/Scripts/index.js
@@ -0,0 +1,81 @@
+﻿$(function () {
+
+    $.get('/home/getallpeople', function(people) {
+        people.forEach(addPersonToTable);
+    });
+
+    $("#addPerson").on('click', function() {
+        $(".addPersonModal").modal();
+    });
+
+    $(".savePerson").on('click', function() {
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+
+        $.post("/home/addperson", {
+                firstName: firstName,
+                lastName: lastName
+            }, function(person) {
+                addPersonToTable(person);
+                $(".addPersonModal").modal('hide');
+                clearModals();
+        });
+    });
+
+    function addPersonToTable(person) {
+        var html = new EJS({ url: '/content/templates/person-row.html' }).render(person);
+        var row = $(html);
+        $("#personsTable").append(row);
+    }
+    
+    $("#personsTable").on('click', '.addCar', function () {
+        var personId = $(this).data('personid');
+        $(".addCarModal").data('personid', personId);
+        $(".addCarModal").modal();
+    });
+
+    $(".saveCar").on('click', function () {
+        var personId = $(".addCarModal").data('personid');
+        var make = $("#make").val();
+        var model = $("#model").val();
+        var year = $("#year").val();
+
+        $.post("/home/addcar", {
+            personId: personId,
+            make: make,
+            model: model,
+            year: year
+        }, function() {
+            $(".addCarModal").modal('hide');
+            clearModals();
+        });
+    });
+
+    $("#personsTable").on('click', '.viewCars', function() {
+        var personId = $(this).data('personid');
+        $.post("/home/getcarsforperson", { personId: personId }, function (cars) {
+            $(".car-table tr:gt(0)").remove();
+            var template = new EJS({ url: '/content/templates/car-row.html' });
+            cars.forEach(function(car) {
+                var html = template.render(car);
+                var row = $(html);
+                $(".car-table").append(row);
+            });
+
+            $(".viewCarsModal").modal();
+        });
+    });
+
+    $("#personsTable").on('click', '.deletePerson', function() {
+        var personId = $(this).data('personid');
+        var row = $(this).parent().parent();
+        $.post("/home/deleteperson", { personId: personId }, function() {
+            row.remove();
+        });
+    });
+
+    function clearModals() {
+        $(".modal input").val('');
+    }
+
+});
